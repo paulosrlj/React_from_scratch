3628c092642bc506b5d20dd1aad85f7821c24c26
Paulo <paulo.leite@academico.ifpb.edu.br>
2022-02-19 22:26:11 -0300

    :wrench: build - Projeto react configurado para html, css e js com webpack

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './style.css'
+
+const App = () => {
+  return <h1>Hello Webpack</h1>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
